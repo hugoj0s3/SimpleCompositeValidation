60c6c1a25e4912d493df583133382c1d90d52794
hugo <hugo86jose@gmail.com>
2018-04-22 16:07:06 -0300

    Auto stash before merge of "master" and "origin/master"

---
+++ src/SimpleCompositeValidation/Base/ICompositeValidation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SimpleCompositeValidation.Base
+{
+	public interface ICompositeValidation<T> : IValidation<T>
+	{
+		bool HasSummaryMessage { get; }
+		string SummaryMessage { get; }
+		IReadOnlyCollection<IValidation> Validations { get; }
+		ICompositeValidation<T> Add<TMember>(IValidation<TMember> validation, Func<T, TMember> member);
+		ICompositeValidation<T> AddForEach<TMember>(IValidation<TMember> validation, Func<T, IEnumerable<TMember>> members);
+		ICompositeValidation<T> Update(string groupName);
+		ICompositeValidation<T> Update(T target, string groupName);
+		ICompositeValidation<T> Update<TMember>(string groupName, TMember value);
+	}
+}
